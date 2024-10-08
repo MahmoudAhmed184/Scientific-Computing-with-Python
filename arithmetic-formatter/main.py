bbc9ed1f66992510cc194beac622ee9f19da6044
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-10-08 04:22:04 +0300

    feat: create main.py to demonstrate arithmetic_arranger functionality

---
+++ arithmetic-formatter/main.py
@@ -0,0 +1,9 @@
+from arithmetic_arranger import arithmetic_arranger
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+
+# Run unit tests automatically
+
+# from pytest import main
+# main(['-vv'])
