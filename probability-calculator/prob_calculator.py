ac17236d47d6b3e28ee93a342b0a8da6b4193489
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-10-12 01:29:03 +0300

    refactor: improve Hat class and experiment function code structure and readability

--- probability-calculator/prob_calculator.py
+++ probability-calculator/prob_calculator.py
@@ -1,34 +1,32 @@
 import copy
 import random
 
-# Consider using the modules imported above.
-
 
 class Hat:
-    def __init__(self, **kwargs):
-        self.contents = [key for key, value in kwargs.items() for _ in range(value)]
+    def __init__(self, **balls):
+        self.contents = [color for color, count in balls.items() for _ in range(count)]
 
-    def draw(self, number_of_balls):
+    def draw(self, number_of_balls: int):
         if number_of_balls > len(self.contents):
-            return self.contents
+            drawn_balls = copy.deepcopy(self.contents)
+            self.contents.clear()
+            return drawn_balls
+        
+        return [
+            self.contents.pop(random.randrange(len(self.contents)))
+            for _ in range(number_of_balls)
+        ]
 
-        drawn_balls = []
-        for _ in range(number_of_balls):
-            drawn_balls.append(self.contents.pop(random.randrange(len(self.contents))))
 
-        return drawn_balls
-
-
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    probability = 0.0
+def experiment(hat: Hat, expected_balls: dict[str, int], num_balls_drawn: int, num_experiments: int) -> float:
+    successful_experiments = 0
     for _ in range(num_experiments):
         hat_copy = copy.deepcopy(hat)
         drawn_balls = hat_copy.draw(num_balls_drawn)
-        successful_experiment = True
-        for key, value in expected_balls.items():
-            if drawn_balls.count(key) < value:
-                successful_experiment = False
-        if successful_experiment:
-            probability += 1 / num_experiments
 
-    return probability
+        success = all(drawn_balls.count(color) >= count for color, count in expected_balls.items())
+
+        if success:
+            successful_experiments += 1
+
+    return successful_experiments / num_experiments
